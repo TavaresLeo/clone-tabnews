ede7c014af98f28e98f11003ad2f8271870b4420
Leonardo Tavares <106924333+TavaresLeo@users.noreply.github.com>
2026-01-16 06:35:38 +0000

    atualizando a frase.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O amor é brisa que arde sem se vergit</h1>
+    return <h1>A amizade é semelhante a um bom café; uma vez frio, não se aquece sem perder bastante do primeiro sabor. </h1>
 }
 
 export default Home;  
