f3697649efaa5b5b6e5060e102af9bfcb14bc51e
Leonardo Tavares <106924333+TavaresLeo@users.noreply.github.com>
2026-02-03 08:11:11 +0000

    adicionar scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>A amizade é semelhante a um bom café; uma vez frio, não se aquece sem perder bastante do primeiro sabor. </h1>
+  return (
+    <h1>
+      A amizade é semelhante a um bom café; uma vez frio, não se aquece sem
+      perder bastante do primeiro sabor.{" "}
+    </h1>
+  );
+}
+function teste() {
+  console.log("indentação errada.");
 }
 
-export default Home;  
+export default Home;
